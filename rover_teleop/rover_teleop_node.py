f2da0fea342c48c3c640c6c35a195dd3dd3bb1c9
sheikhshakibhossain <sheikhshakibpro@gmail.com>
2023-12-11 20:29:27 +0600

    support for logitech extreme 3D pro |tested

--- rover_teleop/rover_teleop_node.py
+++ rover_teleop/rover_teleop_node.py
@@ -23,12 +23,25 @@ class RoverTeleopNode(Node):
 
     def joy_callback(self, msg):
 
-        if msg.buttons[2] == 1 and self.dummyNode is None:
+        # xbox-360 and ps4
+        # if msg.buttons[2] == 1 and self.dummyNode is None:
+        #     self.send_msg = True
+        #     self.create_dummy_node()
+        #     print("X was pressed and executed")
+
+        # elif msg.buttons[1] == 1 and self.dummyNode is not None:  
+        #     self.send_msg = False
+        #     self.destroy_dummy_node()
+        #     print("B was pressed and executed")
+
+
+        # logitech X-3D Pro
+        if msg.buttons[1] and self.dummyNode is None:
             self.send_msg = True
             self.create_dummy_node()
             print("X was pressed and executed")
 
-        elif msg.buttons[1] == 1 and self.dummyNode is not None:  
+        elif msg.buttons[2] and self.dummyNode is not None:  
             self.send_msg = False
             self.destroy_dummy_node()
             print("B was pressed and executed")
@@ -44,10 +57,31 @@ class RoverTeleopNode(Node):
         self.dummyNode = None
 
     def publish_twist_msg(self, joy):
-        # implement variable speed movement here ( from reza )
         twist = Twist()
-        twist.linear.x = joy.axes[1] + joy.axes[4]
-        twist.angular.z = joy.axes[0] + joy.axes[3]
+        # implement variable speed movement here ( from reza )
+        # twist.linear.x = joy.axes[1] + joy.axes[4]
+        # twist.angular.z = joy.axes[0] + joy.axes[3]
+
+        # xbox-360 and ps4
+        # twist.linear.x = (joy.axes[1]/2) + (joy.axes[4]/2)
+        # twist.angular.z = (joy.axes[0]/2) + (joy.axes[3]/2)
+
+        # logitech X-3D Pro
+
+        if joy.buttons[0]:
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+        # if 360 rotate is ON and usual traverse is not ON
+        elif (joy.axes[2] > 0.17 or joy.axes[2] < -0.17) and not (joy.axes[1] > 0.1 or joy.axes[1] < -0.1) :
+            twist.linear.x = 0.0
+            twist.angular.z = joy.axes[2]
+        else:
+            twist.linear.x = joy.axes[1] 
+            twist.angular.z = joy.axes[0] 
+            # remove angluar velocity bound [ -0.1, 0.1 ]
+            if twist.angular.z < 0.1 and twist.angular.z > -0.1 :
+                twist.angular.z = 0.0
+
         self.cmd_vel_publisher.publish(twist)
 
 
